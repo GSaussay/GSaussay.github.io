45c08ac593dea494bdd1330d0e02a1487429d6f7
GSaussay <gsausssay@hawk.iit.edu>
2014-10-03 15:56:04 -0700

    php added!

---
+++ menu.php
@@ -0,0 +1,13 @@
+    <div id="nav">
+      <a href="index.html">Home</a>
+      <a href="Profile.html">Profile</a>
+      <a href="Projects.html">Projects</a>
+      <a href="Passions.html">Passions</a>
+      <a href="Contact.html">Contact</a>
+    </div>
+    <div id="header">
+      <h2 style="margin-left: 1%"> <b>Guillaume SAUSSAY - Portfolio</b> </h2> 
+      </div>    
+
+    <div class="line-separator"></div>
+    <div class ="divider"></div>    
